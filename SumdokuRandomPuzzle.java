8c4eece0b9b2cc88f5aec3e3c3d6462c31ea3e75
MiguelTheDeveloper <miguelcabeca.dev@gmail.com>
2024-12-14 00:07:21 +0000

    SumdokuRandomPuzzle Start
    Begining IP Project

--- SumdokuRandomPuzzle.java
+++ SumdokuRandomPuzzle.java
@@ -1,3 +1,26 @@
+import java.util.Random;
+
 public class SumdokuRandomPuzzle {
-    //
+    
+    private int counter;
+    private Random rand = new Random();
+    SumdokuGrid grid;
+    private int count;
+    private SumdokuGrid[] puzzleArray;
+
+    public SumdokuRandomPuzzle(int size){
+
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public boolean hasNextPuzzle(){
+        return true;
+    }
+
+    public SumdokuPuzzle nextPuzzle(){
+        return null;
+    }
 }
